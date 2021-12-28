c218241a36ab9045edd5da45946a6173e505f8d6
Duncan Schoenmakers <400156@student.fontys.nl>
2021-12-28 14:46:02 +0100

    inital push RESTAPI

---
+++ src/main/java/nowabout/com/blockchainAPI/BlockchainApiApplication.java
@@ -0,0 +1,13 @@
+package nowabout.com.blockchainAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockchainApiApplication.class, args);
+	}
+
+}
